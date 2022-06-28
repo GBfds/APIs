0721d0bdd12f8a43ff6734d98e282aee829551c9
GBfds <fernandesdesousa13@gmail.com>
2022-06-27 22:27:23 -0300

    Update api_basica_com_gin.go

--- api_basica_com_gin.go
+++ api_basica_com_gin.go
@@ -15,9 +15,9 @@ import (
 var db *sql.DB
 
 type pessoa struct {
-	Id        int
-	Nome      string
-	Sobrenome string
+	Id        int    `json:"id"`
+	Nome      string `json:"nome"`
+	Sobrenome string `json:"email"`
 }
 
 func lerBd(c *gin.Context) {
@@ -41,6 +41,23 @@ func lerBd(c *gin.Context) {
 		})
 	}
 }
+func buscarP(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	id := c.Param("id")
+
+	registro := db.QueryRow("SELECT * FROM pessoas WHERE id = ?;", id)
+	var ps pessoa
+	erroScan := registro.Scan(&ps.Id, &ps.Nome, &ps.Sobrenome)
+	if erroScan != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(200, gin.H{
+		"id":        ps.Id,
+		"nome":      ps.Nome,
+		"sobrenome": ps.Sobrenome,
+	})
+}
 func criarP(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 	Body := c.Request.Body
@@ -62,6 +79,41 @@ func criarP(c *gin.Context) {
 		"id":        novaPessoa.Id,
 		"nome":      novaPessoa.Nome,
 		"sobrenome": novaPessoa.Sobrenome,
+	})
+}
+func deletarP(c *gin.Context) {
+	id := c.Param("id")
+
+	_, erroExec := db.Exec("DELETE FROM pessoas WHERE id = ?", id)
+	if erroExec != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.AbortWithStatus(http.StatusOK)
+}
+func atualizarP(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	id := c.Param("id")
+
+	bory := c.Request.Body
+	corpo, erroRead := ioutil.ReadAll(bory)
+	if erroRead != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	var pessoaAtualizada pessoa
+	json.Unmarshal(corpo, &pessoaAtualizada)
+
+	_, erroExec := db.Exec("UPDATE pessoas SET id = ?, nome = ?, sobrenome = ? WHERE id = ?", pessoaAtualizada.Id, pessoaAtualizada.Nome, pessoaAtualizada.Sobrenome, id)
+	if erroExec != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{
+		"id":        pessoaAtualizada.Id,
+		"nome":      pessoaAtualizada.Nome,
+		"sobrenome": pessoaAtualizada.Sobrenome,
 	})
 }
 func princi(c *gin.Context) {
@@ -93,6 +145,9 @@ func main() {
 	r := gin.Default()
 	r.GET("/", princi)
 	r.GET("/pessoas", lerBd)
+	r.GET("/pessoas/:id", buscarP)
+	r.DELETE("pessoas/:id", deletarP)
+	r.PUT("pessoas/:id", atualizarP)
 	r.POST("/pessoas", criarP)
 
 	http.ListenAndServe(":8080", r)
